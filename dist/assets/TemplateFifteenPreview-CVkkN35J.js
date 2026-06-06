9c476ba481b33cafafb820c310b9a12a383ef0e1
auxxweb <auxxweb@gmail.com>
2026-06-06 15:02:07 +0530

    production 3 bug fix

--- dist/assets/TemplateFifteenPreview-CVkkN35J.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./rolldown-runtime-S-ySWqyJ.js";import{i as t,o as n}from"./google-maps-Dmm2uAc5.js";import{TemplateFifteen as r}from"./TemplateFifteen-DPFN1krX.js";var i=e(n(),1),a=t(),o=(0,i.memo)(function(){return(0,a.jsx)(r,{businessData:{}})});export{o as TemplateFifteenPreview};
